469e18d29ec256205a7fae23ee00d321104bc821
Laptop-IQ <laptechmind@gmail.com>
2026-02-27 14:30:17 +0530

    Update BookingsPage.jsx
    add render backend link

--- admin/src/components/BookingsPage.jsx
+++ admin/src/components/BookingsPage.jsx
@@ -8,7 +8,7 @@ import {
   GraduationCap,
 } from "lucide-react";
 
-const API_BASE = "http://localhost:8000";
+const API_BASE = "https://online-course-n9hd.onrender.com";
 
 const BookingsPage = () => {
   const [searchTerm, setSearchTerm] = useState("");
