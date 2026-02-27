b7cb820cc4fea3aa9f365094bf0d3e59244b9513
Laptop-IQ <laptechmind@gmail.com>
2026-02-27 15:57:18 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use("/uploads", express.static("uploads"));
 
 app.use(
   cors({
-    origin: ["https://lms-client-1q2c.onrender.com/", "https://lms-my-project.onrender.com/"],
+    origin: ["https://lms-client-1q2c.onrender.com", "https://lms-my-project.onrender.com"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
