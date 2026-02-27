1c27568e9dcbb157309d9ef1d449a8da8fbfac9d
Laptop-IQ <laptechmind@gmail.com>
2026-02-27 22:38:51 +0530

    Update DasboardPage.jsx

--- admin/src/components/DasboardPage.jsx
+++ admin/src/components/DasboardPage.jsx
@@ -9,7 +9,7 @@ import {
   Search,
 } from "lucide-react";
 
-const API_BASE = "https://lms-my-project-eak1.onrender.com";
+const API_BASE = "https://projectbackend-theta.vercel.app";
 const fmtCurrency = (n) => {
   if (n == null) return "₹0";
   const num = Number(n);
