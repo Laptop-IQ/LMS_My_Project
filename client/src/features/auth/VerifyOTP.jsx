45b752dd7d0cd464754ffbe974a2f2f9498e3f1c
Laptop-IQ <laptechmind@gmail.com>
2026-02-27 16:16:16 +0530

    Update VerifyOTP.jsx

--- client/src/features/auth/VerifyOTP.jsx
+++ client/src/features/auth/VerifyOTP.jsx
@@ -44,7 +44,7 @@ const VerifyOTP = () => {
     try {
       setIsLoading(true);
       const res = await axios.post(
-        `http://localhost:8000/user/verify-otp/${email}`,
+        `https://lms-my-project-eak1.onrender.com/user/verify-otp/${email}`,
         {
           otp: finalOtp,
         },
