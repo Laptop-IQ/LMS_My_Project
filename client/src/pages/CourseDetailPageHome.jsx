70ae2242f89d0867c7523b38c6b952da15398452
Laptop-IQ <laptechmind@gmail.com>
2026-02-27 22:46:21 +0530

    Update CourseDetailPageHome.jsx

--- client/src/pages/CourseDetailPageHome.jsx
+++ client/src/pages/CourseDetailPageHome.jsx
@@ -25,7 +25,7 @@ import {
 import { TOKEN_KEY } from "@/constants/auth";
 
 
-const API_BASE = "https://lms-my-project-eak1.onrender.com";
+const API_BASE = "https://projectfrontend-wheat.vercel.app";
 
 const fmtMinutes = (mins) => {
   const h = Math.floor((mins || 0) / 60);
